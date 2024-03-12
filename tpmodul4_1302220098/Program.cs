e3249e8b67f1b71c1de413a0eca986ef70b74f4e
unknown <haikalalfaro123@gmail.com>
2024-03-13 00:03:59 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302220098/Program.cs
+++ tpmodul4_1302220098/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using tpmodul4_1302220098;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // MEMANGGIL DATA DARI CLASS KODEPOS
+        KodePos DataKodePos = new KodePos();
+
+        String kodePosWates = DataKodePos.GetKodePos("Wates");
+        String kodePosCijaura = DataKodePos.GetKodePos("Cijaura");
+        String kodePosMargasari = DataKodePos.GetKodePos("Margasari");
+        Console.WriteLine("INI SOAL NOMOR 1");
+        Console.WriteLine("ini kode post Wates: " + kodePosWates);
+        Console.WriteLine("ini kode post Cijaura: " + kodePosCijaura);
+        Console.WriteLine("ini kode post Margasari: " + kodePosMargasari);
+        Console.WriteLine(" ");
+    }
+}
+
--- tpmodul4_1302220098/obj/Debug/net8.0/tpmodul4_1302220098.AssemblyInfo.cs
+++ tpmodul4_1302220098/obj/Debug/net8.0/tpmodul4_1302220098.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_1302220098")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ddd24be15446e2f398158ef3aa56289c581aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_1302220098")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_1302220098")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
